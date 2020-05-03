1d9f8279644f27836ca89c4a7beb14d978cc306c
Elias <EliasEmail>
2020-05-03 12:13:29 +0200

    MainMenu and options done: only volume

--- Dolphin Space Hunter/Assets/_Scripts/MainMenu.cs
+++ Dolphin Space Hunter/Assets/_Scripts/MainMenu.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject musicManager;
+
     public void PlayGame()
     {
         SceneManager.LoadScene("PlayGameScene");
@@ -17,10 +19,18 @@ public class MainMenu : MonoBehaviour
         
     }
 
+     public void Awake () {
+      var go = GameObject.Find("Music"); //Finds the game object called Game Music.
+      if (go != musicManager) { 
+          go = musicManager; //Replaces the old audio with the new one set in the inspector.
+          go.GetComponent<AudioSource>().Stop(); //Plays the audio.
+      }
+    }
     
     public void OptionsGame()
     {
+        DontDestroyOnLoad(musicManager);   
         SceneManager.LoadScene("OptionsScene");
-        
+       
     }
 }
