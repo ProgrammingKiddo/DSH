861f3b7f9e4eb71f562f02ca873ce559a1c44f59
ProgrammingKiddo <programming.edu@protonmail.com>
2020-05-10 20:09:28 +0200

    Mezclado el progreso de la escena de disparo con el de la de asteroides.

--- Dolphin Space Hunter/Assets/_Scripts/MainMenu.cs
+++ Dolphin Space Hunter/Assets/_Scripts/MainMenu.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    public GameObject musicManager;
-
     public void PlayGame()
     {
-        SceneManager.LoadScene("PlayGameScene");
+        SceneManager.LoadScene("AsteroidScene");
 
     }
 
@@ -19,18 +17,10 @@ public class MainMenu : MonoBehaviour
         
     }
 
-     public void Awake () {
-      var go = GameObject.Find("Music"); //Finds the game object called Game Music.
-      if (go != musicManager) { 
-          go = musicManager; //Replaces the old audio with the new one set in the inspector.
-          go.GetComponent<AudioSource>().Stop(); //Plays the audio.
-      }
-    }
     
     public void OptionsGame()
     {
-        DontDestroyOnLoad(musicManager);   
         SceneManager.LoadScene("OptionsScene");
-       
+        
     }
 }
