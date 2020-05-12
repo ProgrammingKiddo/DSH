8a2ff1f3c8ddbf2319514f2dffae595a077b34b5
ProgrammingKiddo <programming.edu@protonmail.com>
2020-05-12 22:52:40 +0200

    Añadido un paquete de sfx y naves espaciales temporales.

---
+++ Dolphin Space Hunter/Assets/_Scripts/Shooting_Scripts/ShootProjectile.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Copyright (c) Borja Fernández
+ *
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour
+{
+
+    #region Variables
+    public GameObject gunMuzzle;
+    public GameObject target;
+    #endregion
+
+
+    #region UnityMethods
+
+    void Start()
+    {
+        this.transform.LookAt(target.transform);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    public void Shoot(GameObject projectile)
+    {
+        GameObject projectileShot = Instantiate(projectile, gunMuzzle.transform.position, this.transform.rotation);
+        //projectileShot.transform.LookAt(target.transform);
+        Debug.Log("Target:" + target.transform.position + ", projectileForward: " + this.transform.forward);
+        projectileShot.GetComponent<Rigidbody>().velocity = projectile.transform.forward * projectile.GetComponent<ProjectileScript>().projectileSpeed;
+    }
+}
