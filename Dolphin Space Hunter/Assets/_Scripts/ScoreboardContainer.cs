947b4a8c054ff1f174fbbcdca09eca691ad22113
ProgrammingKiddo <programming.edu@protonmail.com>
2020-05-17 22:17:26 +0200

    Añadidos los scripts para almacenamiento y carga/descarga de la puntuación.

---
+++ Dolphin Space Hunter/Assets/_Scripts/ScoreboardContainer.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Copyright (c) Borja Fernández
+ *
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreboardContainer : MonoBehaviour
+{
+
+    #region Variables
+    public string difficultyMode;
+    public List<string> players = new List<string>();
+    public List<int> scores = new List<int>();
+    #endregion
+
+
+    #region UnityMethods
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
---
+++ Dolphin Space Hunter/Assets/_Scripts/ScoreboardManager.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Copyright (c) Borja Fernández
+ *
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreboardManager : MonoBehaviour
+{
+
+    public static void loadScoreboard(object obj)
+    {
+        string jsonText = JsonUtility.ToJson(obj);
+        Debug.Log(jsonText);
+    }
+   
+}
