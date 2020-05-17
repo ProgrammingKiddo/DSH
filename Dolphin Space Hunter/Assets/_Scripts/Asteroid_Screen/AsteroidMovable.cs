91ea871c65df869de549492787f8eb04d77918d3
unknown <sergioruizpino14@gmail.com>
2020-05-17 13:52:08 +0200

    Añadido cambios a la escena asteroides, completada al 90%

---
+++ Dolphin Space Hunter/Assets/_Scripts/Asteroid_Screen/AsteroidMovable.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Copyright (c) Sergio Ruiz
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using Vuforia;
+using UnityEngine;
+
+public class AsteroidMovable : MonoBehaviour
+{
+    public GameObject Atype1, Atype2, Atype3;
+    private GameObject asteroid;
+    private float initX, initY, initZ = 2000;
+    public Camera MyCamera;
+    public float vel,radio;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("createAsteroid");
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    IEnumerator createAsteroid()
+    {
+        while (true)
+        {
+            int tipoA = Random.Range(0, 3);
+            initX = Random.Range(-Screen.width/2,Screen.width / 2);
+            initY = Random.Range(-Screen.height/2, Screen.height/2);
+            initX += MyCamera.transform.position.x;
+            initY += MyCamera.transform.position.y;
+            switch (tipoA)
+            {
+                case 0: asteroid = Atype1;break;//Instantiate(Atype1, new Vector3(initX, initY, initZ), Quaternion.identity, imagen.transform); break;  //genera asteroides
+                case 1: asteroid = Atype2;break;// Instantiate(Atype2, new Vector3(initX, initY, initZ), Quaternion.identity, imagen.transform); break;
+                case 2: asteroid = Atype3;break;// Instantiate(Atype3, new Vector3(initX, initY, initZ), Quaternion.identity, imagen.transform); break;
+                default: break;
+            }
+            asteroid = Instantiate(asteroid, new Vector3(initX, initY, initZ), Quaternion.identity);
+            asteroid.GetComponent<Rigidbody>().velocity = new Vector3(0f,0f,vel);
+            asteroid.transform.localScale = new Vector3(75f, 75f, 75f);//tam asteroides DIFICULTA cambiar
+            asteroid.GetComponent<SphereCollider>().radius = radio;
+            yield return new WaitForSecondsRealtime(1.0f);
+        }
+
+    }
+
+  /*  IEnumerator movAsteroid()
+    {
+        //childObject.transform.parent.gameObject
+        initZ -= vel; //vel
+        asteroid.transform.Translate(0, 0, initZ);
+        if (asteroid.transform.position.z <= limit && this.transform.parent.gameObject == imagen.gameObject)
+        {
+            Debug.Log("destruido");
+            Destroy(asteroid);
+            Destroy(this.gameObject);
+        }
+        if (asteroid.transform.position.z <= limit && this.transform.parent.gameObject != imagen.gameObject)
+        {
+            Debug.Log("parado");
+           // Destroy(asteroid);
+
+        }
+        yield return new WaitForSeconds(.1f);
+    }
+    */
+
+
+
+
+}
---
+++ Dolphin Space Hunter/Assets/_Scripts/Asteroid_Screen/CameraMovable.cs
@@ -0,0 +1,121 @@
+﻿/*
+ * Copyright (c) Sergio Ruiz
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CameraMovable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera ArCamera;
+    float IntervaloAc = 1.0f / 30; //valor actualizacion;
+    float LowPassKernelWidthInSeconds = 1.0f;
+    float factorFiltro = 0;
+    public GUISteroid canvasVida;
+    private bool sinEscudo;
+    private int actual;
+    AudioSource sonidoChoque;
+    Vector3 valorCrudo = Vector3.zero;
+
+    void Start()
+    {
+        canvasVida.golpeado(100);
+        actual = 100;
+        factorFiltro = IntervaloAc / LowPassKernelWidthInSeconds;
+        valorCrudo = Input.acceleration;
+        sonidoChoque = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        actualizaCamara();
+        
+    }
+
+    private Vector3 FiltradoAccelValor()
+    {
+        Debug.Log("NO FILTRADO " + Input.acceleration);
+        Debug.Log("fILTREADO " + Vector3.Lerp(valorCrudo, Input.acceleration, factorFiltro));
+        return Vector3.Lerp(valorCrudo, Input.acceleration, factorFiltro);
+     
+    }
+    private void actualizaCamara()
+    {
+        float speed = 200.0f;
+        Vector3 prueba  = Vector3.zero;
+
+        // we assume that the device is held parallel to the ground
+        // and the Home button is in the right hand
+
+        // remap the device acceleration axis to game coordinates:
+        // 1) XY plane of the device is mapped onto XZ plane
+        // 2) rotated 90 degrees around Y axis
+        prueba.x = -Input.acceleration.y;
+        prueba.z = Input.acceleration.x;
+
+        // clamp acceleration vector to the unit sphere
+        if (prueba.sqrMagnitude > 1)
+            prueba.Normalize();
+
+        prueba *= Time.deltaTime;
+        prueba *= speed;
+        Vector3 newPosition = new Vector3(ArCamera.transform.position.x + prueba.z, ArCamera.transform.position.y + prueba.x, 0);
+
+
+        // Move object
+        // ArCamera.gameObject.Translate(prueba * speed);
+        ArCamera.transform.position= newPosition;
+
+
+
+
+      /*  Vector3 acelerometroValor = FiltradoAccelValor();
+        Vector3 newPosition = new Vector3(ArCamera.transform.position.x + acelerometroValor.x,ArCamera.transform.position.y+acelerometroValor.y, ArCamera.transform.position.z);
+        ArCamera.transform.position = newPosition;
+        Debug.Log(ArCamera.transform.position);*/
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("NO FILTRADO " + Input.acceleration + " Fil " + Vector3.Lerp(valorCrudo, Input.acceleration, factorFiltro)+"Camera "+ArCamera.transform.position);
+
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("cOLISSION");
+        if (other.gameObject.tag == "Enemy")
+        {
+            sonidoChoque.Play();
+            actual -= 10; //facil
+            canvasVida.golpeado(actual);
+            if (actual <= 0)
+            {
+                if (sinEscudo)
+                {
+                    SceneManager.LoadScene("gameOver");//cargar GAME OVER
+
+                }
+                else
+                {
+                    sinEscudo = true;
+                }
+            }
+        }
+
+    }
+
+}
+
