7883f35f78fdd3b9ebfe345d1e673472f9dd16ce
Elias <EliasEmail>
2020-05-19 20:43:17 +0200

    Intro video done.

--- Dolphin Space Hunter/Assets/_Scripts/MainMenu.cs
+++ Dolphin Space Hunter/Assets/_Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("AsteroidScene");
+        SceneManager.LoadScene("IntroVideoScene");
 
     }
 
---
+++ Dolphin Space Hunter/Assets/_Scripts/my_intro_video.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class my_intro_video : MonoBehaviour
+{
+    private VideoPlayer video;
+    // Start is called before the first frame update
+   void Start() 
+     {
+         video=this.GetComponent<VideoPlayer>();
+          video.loopPointReached += LoadScene;
+     }
+     void LoadScene(VideoPlayer vp)
+     {
+          SceneManager.LoadScene( "AsteroidScene" );
+      }
+}
+
