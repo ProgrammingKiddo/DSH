df3c97f9ae0e6173a10f381118dbfd083c82089a
YisGG <jgalvez.jgg@gmail.com>
2020-05-20 20:03:07 +0200

    Cambio icono escudo

--- Dolphin Space Hunter/Assets/_Scripts/Shield_Recharge/ShieldScript.cs
+++ Dolphin Space Hunter/Assets/_Scripts/Shield_Recharge/ShieldScript.cs
@@ -7,7 +7,7 @@ public class ShieldScript : MonoBehaviour
     // Start is called before the first frame update
     public RectTransform rectTransform;
     public static float Shield{get; set;}
-    
+
     // Start is called before the first frame update
     void Start()
     {
