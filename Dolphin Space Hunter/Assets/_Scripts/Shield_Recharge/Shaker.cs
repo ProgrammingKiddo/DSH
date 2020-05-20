232f159ecd309f716e2a6352a3ed5f0e631f5951
YisGG <jgalvez.jgg@gmail.com>
2020-05-20 19:16:20 +0200

    Primer prototipo pantalla escudo

---
+++ Dolphin Space Hunter/Assets/_Scripts/Shield_Recharge/Shaker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaker : MonoBehaviour
+{
+    public float sensibility;
+    public float rechargeRate;
+    private int cont;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cont = 0;
+        sensibility = 5;
+        rechargeRate = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Shield = " + ShieldScript.Shield + "\ncont = " + cont);
+        //Debug.Log("X:" + Input.acceleration.x + " Y:" + Input.acceleration.y + " Z:" + Input.acceleration.z + " Sum:" + 
+        //          (Mathf.Abs(Input.acceleration.x) + Mathf.Abs(Input.acceleration.y) + Mathf.Abs(Input.acceleration.z)));
+        if(ShieldScript.Shield < 100){
+            if((Mathf.Abs(Input.acceleration.x) + Mathf.Abs(Input.acceleration.y) + Mathf.Abs(Input.acceleration.z)) > sensibility){
+                cont++;
+                if(cont >= rechargeRate){
+                    ShieldScript.Shield++;
+                    cont = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Dolphin Space Hunter/Assets/_Scripts/Shield_Recharge/ShieldScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public RectTransform rectTransform;
+    public static float Shield{get; set;}
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Shield = PlayerPrefs.GetFloat("Shield");
+        Shield = 0f;
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float UpdateShield = Mathf.MoveTowards(rectTransform.rect.height, Shield, 5.0f);
+        rectTransform.sizeDelta = new Vector2(100f, Mathf.Clamp(UpdateShield, 0.0f, 100f));
+    }
+}
