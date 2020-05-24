0af8be2332257182c040bac66af076d2349ab52d
YisGG <jgalvez.jgg@gmail.com>
2020-05-24 23:19:25 +0200

    Conexiones basicas de parametros del juego para las pantallas de escudo y recarga de municion

--- Dolphin Space Hunter/Assets/_Scripts/Ammunition_Reload/AmmunitionReloadScript.cs
+++ Dolphin Space Hunter/Assets/_Scripts/Ammunition_Reload/AmmunitionReloadScript.cs
@@ -2,33 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class AmmunitionReloadScript : MonoBehaviour
 {
-    public int indicatorSpeed = 250;
-    public Text ammunitionCounter;
+    public int indicatorSpeed;
+    public Text ammunitionCounter, scorePanel;
+    public ProgressBar shieldBar;
 
     private int absMax = 49;
+    private int currentAmmunition, maxAmmunition, currentScore;
+    private float currentShield;
     //Color Ranges:
     //-49[RED]-45[YELLOW]-6[GREEN]5[YELLOW]41[RED]49
-    private int[] colorRanges = {-45, -6, 5, 41};
+    private int[] colorBarRanges = {-45, -6, 5, 41};
     // Start is called before the first frame update
     void Start()
     {
+        currentAmmunition = PlayerPrefs.GetInt("Ammo", 0);
+        maxAmmunition = PlayerPrefs.GetInt("MaxAmmo", 50);
+        currentShield = PlayerPrefs.GetFloat("Shield", 100f);
+        currentScore = PlayerPrefs.GetInt("Score", 0);
+        indicatorSpeed = PlayerPrefs.GetInt("reloadBarSpeed", 250);
+
+        shieldBar.BarValue = currentShield;
+        ammunitionCounter.text = currentAmmunition.ToString() + "/" + maxAmmunition.ToString();
+        setAmmounitionCounterColor();
+        scorePanel.text = "Score: " + currentScore.ToString();
+
         StartCoroutine(reloadAction());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space")){
-            if(transform.localPosition.x <=-45 || transform.localPosition.x>=41){
-                Debug.Log("RecargaRoja");
-            }else if(transform.localPosition.x <=-6 || transform.localPosition.x>=5){
-                Debug.Log("RecargaAmarilla");
+        //if(Input.GetKeyDown("space") && currentAmmunition<maxAmmunition){
+        if(Input.touchCount==1 && Input.GetTouch(0).phase == TouchPhase.Began && currentAmmunition<maxAmmunition){
+            if(transform.localPosition.x<=colorBarRanges[0] || transform.localPosition.x>=colorBarRanges[3]){
+                currentAmmunition += (maxAmmunition/10);    //Red reload => +10% of ammo
+            }else if(transform.localPosition.x <=colorBarRanges[1] || transform.localPosition.x>=colorBarRanges[2]){
+                currentAmmunition += (maxAmmunition/4);     //Yellow reload => +25% of ammo
             }else{
-                Debug.Log("RecargaVerde");
+                currentAmmunition += (maxAmmunition/2);     //Green reload => +50% of ammo
             }
+            if(currentAmmunition > maxAmmunition){
+                currentAmmunition = maxAmmunition;
+            }
+            ammunitionCounter.text = currentAmmunition.ToString() + "/" + maxAmmunition.ToString();
+            setAmmounitionCounterColor();
         }
     }
 
@@ -48,6 +69,16 @@ public class AmmunitionReloadScript : MonoBehaviour
             }
             
             yield return new WaitForSecondsRealtime(0.01f);
+        }
+    }
+
+    void setAmmounitionCounterColor(){
+        if(currentAmmunition <= (maxAmmunition*0.1)){
+            ammunitionCounter.color = Color.red;
+        }else if(currentAmmunition <= (maxAmmunition*0.75)){
+            ammunitionCounter.color = Color.yellow;
+        }else{
+            ammunitionCounter.color = Color.green;
         }
     }
 }
--- Dolphin Space Hunter/Assets/_Scripts/Shield_Recharge/ShieldScript.cs
+++ Dolphin Space Hunter/Assets/_Scripts/Shield_Recharge/ShieldScript.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShieldScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public RectTransform rectTransform;
+    public Text scorePanel, ammunitionCounter;
     public static float Shield{get; set;}
+    private int currentScore, currentAmmunition, maxAmmunition;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Shield = PlayerPrefs.GetFloat("Shield");
-        Shield = 0f;
+        Shield = PlayerPrefs.GetFloat("Shield", 0f);
+        currentAmmunition = PlayerPrefs.GetInt("Ammo", 0);
+        maxAmmunition = PlayerPrefs.GetInt("maxAmmo", 50);
+        currentScore = PlayerPrefs.GetInt("Score", 0);
+        
+        scorePanel.text = "Score: " + currentScore.ToString();
+        ammunitionCounter.text = currentAmmunition.ToString() + "/" + maxAmmunition.ToString();
+        setAmmounitionCounterColor();
+
         rectTransform = GetComponent<RectTransform>();
     }
 
@@ -21,5 +31,15 @@ public class ShieldScript : MonoBehaviour
     {
         float UpdateShield = Mathf.MoveTowards(rectTransform.rect.height, Shield, 5.0f);
         rectTransform.sizeDelta = new Vector2(100f, Mathf.Clamp(UpdateShield, 0.0f, 100f));
+    }
+
+    void setAmmounitionCounterColor(){
+        if(currentAmmunition <= (maxAmmunition*0.1)){
+            ammunitionCounter.color = Color.red;
+        }else if(currentAmmunition <= (maxAmmunition*0.75)){
+            ammunitionCounter.color = Color.yellow;
+        }else{
+            ammunitionCounter.color = Color.green;
+        }
     }
 }
